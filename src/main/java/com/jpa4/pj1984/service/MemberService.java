68570474d2b6327c7aca62a509939ca0fb6a48b5
hanouw <jehasite@gmail.com>
2024-04-01 16:10:10 +0900

    Member, MC, MDTO, MS, signup(관리자, 회원) : 회원가입 기능 및 html 수정

--- src/main/java/com/jpa4/pj1984/service/MemberService.java
+++ src/main/java/com/jpa4/pj1984/service/MemberService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -17,5 +19,15 @@ public class MemberService {
     public Member save(MemberDTO memberDTO){
         Member memberSaved = memberRepository.save(memberDTO.toEntity());
         return memberSaved;
+    }
+
+    public MemberDTO login(MemberDTO memberDTO){
+//        Optional<Member> dbMember = memberRepository.findById(memberDTO.getUser_no());
+        Member dbMember = memberRepository.findById(memberDTO.getUser_no()).orElse(null);
+//        if(dbMember.isPresent()) { // Optional 객체가 값을 가지고 있다면 true, 값이 없다면 false 리턴
+////            Member member = findMember.filter(m -> m.getPassword().equals(memberDTO.getPassword())).orElse(null);
+//            dbMember.getUser_password().equals(memberDTO.getUser_password())
+//        }
+        return null;
     }
 }
