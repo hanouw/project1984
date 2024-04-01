3d5c1e495caa01f726243506e7283797a709351c
sisyulmu <selly117@naver.com>
2024-04-01 11:59:48 +0900

    BC,CMSC,HC,MC,OC,SC,html:파일추가

---
+++ src/main/java/com/jpa4/controller/StoreController.java
@@ -0,0 +1,22 @@
+package com.jpa4.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/store")
+public class StoreController {
+
+    @GetMapping("/add")
+    public String addForm() {
+        log.info("***** ");
+        return "";
+    }
+
+
+}
