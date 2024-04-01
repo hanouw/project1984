f023d7f79ecfaa6dcff59054cc0273e844ebc863
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-04-01 11:09:12 +0900

    0401_YOO 북컨트롤러 추가

---
+++ src/main/java/com/jpa4/pj1984/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.jpa4.pj1984.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/crm")
+@Slf4j
+public class BookController {
+    //상품리스트
+
+}
