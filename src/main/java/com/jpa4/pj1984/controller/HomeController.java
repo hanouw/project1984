5ac1a2e4a01bf93630198d40a190b162d7f5601f
sisyulmu <selly117@naver.com>
2024-04-01 12:09:07 +0900

    HC,OC 수정

--- src/main/java/com/jpa4/pj1984/controller/HomeController.java
+++ src/main/java/com/jpa4/pj1984/controller/HomeController.java
@@ -1,25 +1,20 @@
 package com.jpa4.pj1984.controller;
 
-<<<<<<< HEAD
+
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 @RequestMapping("/")
 public class HomeController {
-=======
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
 
     @GetMapping("/")
     public String homeController(){
         return "home";
     }
->>>>>>> origin/main
+
 }
--- src/main/java/com/jpa4/pj1984/controller/OrderController.java
+++ src/main/java/com/jpa4/pj1984/controller/OrderController.java
@@ -1,6 +1,6 @@
 package com.jpa4.pj1984.controller;
 
-<<<<<<< HEAD
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Slf4j
 @RequestMapping("/order")
-=======
->>>>>>> origin/main
+
 public class OrderController {
 }
