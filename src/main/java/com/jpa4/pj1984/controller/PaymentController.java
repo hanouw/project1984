260e43956576ede6adf17e82f8e9300aec67d1b8
Goldenhyo <jintop01@naver.com>
2024-04-02 12:45:13 +0900

    Payment 관련 domain, PaymentController : 추가

--- src/main/java/com/jpa4/pj1984/controller/PaymentController.java
+++ src/main/java/com/jpa4/pj1984/controller/PaymentController.java
@@ -1,13 +1,56 @@
 package com.jpa4.pj1984.controller;
 
 
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.io.IOException;
+
+@RestController
 @Slf4j
 @RequestMapping("/order")
-
 public class PaymentController {
+
+    @Value("${iamport.key}")
+    private String restApiKey;
+    @Value("${iamport.secret}")
+    private String restApiSecret;
+
+    private IamportClient iamportClient;
+
+    @PostConstruct
+    public void init() {
+        this.iamportClient = new IamportClient(restApiKey, restApiSecret);
+    }
+
+//    @PostMapping("/book")
+//    public ResponseEntity<String> paymentComplete(@Login SessionUser sessionUser, @RequestBody List<OrderSaveDto> orderSaveDtos) throws IOException {
+//        String orderNumber = String.valueOf(orderSaveDtos.get(0).getOrderNumber());
+//        //try {
+//        Long userId = sessionUser.getUserIdNo();
+//        paymentService.saveOrder(userId, orderSaveDtos);
+//        log.info("결제 성공 : 주문 번호 {}", orderNumber);
+//        return ResponseEntity.ok().build();
+        //}
+//        catch (RuntimeException e) {
+//            log.info("주문 상품 환불 진행 : 주문 번호 {}", orderNumber);
+//            String token = refundService.getToken(apiKey, secretKey);
+//            refundService.refundWithToken(token, orderNumber, e.getMessage());
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+
+
+
+    @PostMapping("/book/validation/{imp_uid}")
+    @ResponseBody
+    public IamportResponse<Payment> paymentByImpUid(@PathVariable("imp_uid") String imp_uid) throws IOException {
+        return iamportClient.paymentByImpUid(imp_uid);
+    }
+
 }
--- src/main/java/com/jpa4/pj1984/domain/PaymentBookHistory.java
+++ src/main/java/com/jpa4/pj1984/domain/PaymentBookHistory.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Entity
 @Getter @Setter
-public class OrderBookHistory {
+public class PaymentBookHistory {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long OrderBookHistoryId;
     @ManyToOne(fetch = FetchType.LAZY)
@@ -15,5 +15,5 @@ public class OrderBookHistory {
     private Book book;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_book_id")
-    private Order order;
+    private Payment payment;
 }
