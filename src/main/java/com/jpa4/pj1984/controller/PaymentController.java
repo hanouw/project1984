79f2f2a8b20526193e1d97681500cfe0be74d52c
Goldenhyo <jintop01@naver.com>
2024-04-03 11:19:03 +0900

    build.gradle, Payment : 수정

--- src/main/java/com/jpa4/pj1984/controller/PaymentController.java
+++ src/main/java/com/jpa4/pj1984/controller/PaymentController.java
@@ -1,7 +1,7 @@
 package com.jpa4.pj1984.controller;
 
 
-import com.jpa4.pj1984.DTO.PaymentDTO;
+import com.jpa4.pj1984.dto.PaymentDTO;
 import com.jpa4.pj1984.service.PaymentService;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.response.IamportResponse;
@@ -11,12 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @Slf4j
@@ -43,20 +40,7 @@ public class PaymentController {
                                                   // @AuthenticationPrincipal CustomMember customMember
     ) throws IOException {
 
-        // paymentDTO : orderBookId, userNo, orderBookMethod, paymentBookStatus, createDate, selectedBooks
-        Long userNo = paymentDTO.getUserNo();
-        // PaymentBookHistoryDTO 를 만들고,
-
-        // PaymentEntity 에 주입?
-
-        // paymentService 시켜서 저장하고
-
-        // 주문내역아이디 리턴
-
-        // 화면에 주문내역 뿌리기
-
-        // String orderNumber = String.valueOf(paymentDTO.get(0).getOrderNumber());
-        // Long userNo = customMember.getUserNo();
+        Long userNo = paymentDTO.getUserNo(); // TODO 추후 수정
         paymentService.saveOrder(userNo, paymentDTO);
         // log.info("결제 성공 : 주문 번호 {}", orderNumber);
         return ResponseEntity.ok().build();
