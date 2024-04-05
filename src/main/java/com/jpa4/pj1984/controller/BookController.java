6846704c7fd3f9479d760274a8b9274bc1425a92
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-04-05 16:16:06 +0900

    0405_YOO 이미지 넣으면 그냥 폼이 사라져버리는 드래그앤드랍 구현

--- src/main/java/com/jpa4/pj1984/controller/BookController.java
+++ src/main/java/com/jpa4/pj1984/controller/BookController.java
@@ -9,14 +9,19 @@ import com.jpa4.pj1984.dto.BookDTO;
 import com.jpa4.pj1984.dto.BookForm;
 import com.jpa4.pj1984.service.BookCategoryService;
 import com.jpa4.pj1984.service.BookService;
+import com.jpa4.pj1984.service.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,35 +33,46 @@ public class BookController {
     //--공통사용-----------------------------------------------------//
     private final BookService bookService;
     private final BookCategoryService bookCategoryService;
+    private final FileUploadService fileUploadService;
     
-//    @ModelAttribute("categoryLists")
-//    public List<BookCategory> categoryLists(){
+    @ModelAttribute("categoryLists")
+    public List<BookCategory> categoryLists(){
+//        List<BookCategoryDTO> categoryDTOLists = bookCategoryService.findAll();
 //        List<BookCategory> categoryLists = new ArrayList<>();
-//        System.out.println("categoryLists = " + categoryLists);
-//    }
+//        categoryLists.add()
+        List<BookCategoryDTO> bookCategoryDTOList = bookCategoryService.findCategoryAllList();
+
+        return null;
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{fileName}")
+    public Resource getImages(@PathVariable("fileName") String fileName) throws MalformedURLException{
+        return new UrlResource("file:" + fileUploadService.getPath(fileName));
+    }
     
     //--상품관리-----------------------------------------------------//
     
     //상품리스트
     @GetMapping("/book")
-    public String bookList(){
+    public String bookList(Model model){
         log.info("--CMS--Book--List--Request--");
+        List<BookDTO> bookDTOList = bookService.findAll();
+        model.addAttribute("bookList", bookDTOList);
+        System.out.println("bookDTOList = " + bookDTOList);
         return "backend/book/list";
     }
     //상품추가폼
     @GetMapping("/book/add")
     public String bookAddForm(@ModelAttribute BookForm bookForm, Model model){
         log.info("--CMS--Book--AddForm--Request--");
-        List<BookCategoryDTO> bookCategoryDTOList = bookCategoryService.findAll();
-        System.out.println("bookCategoryDTOList = " + bookCategoryDTOList);
-        System.out.println("bookForm = " + bookForm + ", model = " + model);
         return "backend/book/add";
     }
     //상품추가
     @PostMapping("/book/add")
-    public String bookAddPro(BookForm bookForm){
+    public String bookAddPro(BookForm bookForm, MultipartFile file){
         log.info("--CMS--Book-Add--Request--");
-        Long save = bookService.save(bookForm);
+        Long save = bookService.save(bookForm, file);
         return "redirect:/cms/book";
     }
 
--- src/main/java/com/jpa4/pj1984/service/BookService.java
+++ src/main/java/com/jpa4/pj1984/service/BookService.java
@@ -2,12 +2,18 @@ package com.jpa4.pj1984.service;
 
 import com.jpa4.pj1984.domain.Book;
 
+import com.jpa4.pj1984.domain.BookCategory;
+import com.jpa4.pj1984.dto.BookCategoryDTO;
 import com.jpa4.pj1984.dto.BookDTO;
 import com.jpa4.pj1984.dto.BookForm;
 import com.jpa4.pj1984.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -15,11 +21,24 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final FileUploadService fileUploadService;
 
     //저장
-    public Long save(BookForm bookForm){
+    public Long save(BookForm bookForm, MultipartFile file){
         Book bookSaved = bookRepository.save(bookForm.toEntity());
+        //시스템상 유저 경로를 담아줌
+        String projectPath = System.getProperty("user.dir");
+
         return bookSaved.getIsbn();
+    }
+
+    //목록조회
+    public List<BookDTO> findAll() {
+        List<Book> all = bookRepository.findAll();
+        List<BookDTO> list = all.stream()
+                .map(b -> new BookDTO(b))
+                .collect(Collectors.toList());
+        return list;
     }
 
 }
