fc10f81210dbd495ff54f69e2118482a210d6f98
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-04-09 10:06:26 +0900

    0409_YOO 문의 뼈대제작 진행중...

---
+++ src/main/java/com/jpa4/pj1984/repository/InquiryRepository.java
@@ -0,0 +1,7 @@
+package com.jpa4.pj1984.repository;
+
+import com.jpa4.pj1984.domain.Inquiry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+}
